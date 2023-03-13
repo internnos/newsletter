48e903d5af000946a9b336bdd8e4fc74167ddeb0
internos <amajidsinar@gmail.com>
2023-03-13 20:51:22 +0700

    refactor(health_check_works): randomize port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,27 @@
+use std::net::TcpListener;
+
 use zero2prod;
 use reqwest;
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app(endpoint: &str) -> String {
+    let listener = TcpListener::bind(format!("{endpoint}:0")).expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("{endpoint}:{port}").to_string();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    address
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let endpoint = "127.0.0.1";
+    let address = spawn_app(endpoint);
+    println!("address: {address}");
 
     let client = reqwest::Client::new();
 
     let response = client
-    .get("http://127.0.0.1:8000/health_check")
+    .get(format!("http://{address}/health_check"))
     .send()
     .await
     .expect("Failed to execute request");

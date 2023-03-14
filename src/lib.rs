82eb4e7b85bab183ffe166e7a1ee0958d0b77f62
internos <amajidsinar@gmail.com>
2023-03-15 02:08:08 +0700

    feat(subscriptions): new route

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ use std::net::TcpListener;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
@@ -12,6 +17,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new( || {
         App::new()
         .route("/health_check", web::get().to(health_check))
+        .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
+use actix_web::rt::spawn;
 use zero2prod;
 use reqwest;
 
@@ -28,4 +29,23 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_data() {
+    let endpoint = "127.0.0.1";
+    let address = spawn_app(endpoint);
+
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+    .post(format!("http://{address}/subscriptions"))
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .body(body)
+    .send()
+    .await
+    .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
 }

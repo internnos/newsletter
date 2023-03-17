348bf5058ff04dba61d2cc4774c771283922eda1
internos <amajidsinar@gmail.com>
2023-03-18 00:47:18 +0700

    test(subscribe_returns_400_for_missing_data): add

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -49,3 +49,34 @@ async fn subscribe_returns_200_for_valid_data() {
 
     assert_eq!(200, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_data() {
+    let endpoint = "127.0.0.1";
+    let address = spawn_app(endpoint);
+
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, message) in test_cases {
+        let response = client
+        .post(format!("http://{address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(invalid_body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(
+        400, 
+        response.status().as_u16(),
+        "The API did not fail with 400 bad request when the payload was {message}"
+    );
+    }
+
+}
